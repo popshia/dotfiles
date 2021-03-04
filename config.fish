20b621f6ede182d40e58e631b9cdafbb389c02d8
popshia <B123292179@gmail.com>
2021-03-04 14:38:58 +0800

    add fish config

---
+++ config.fish
@@ -0,0 +1,14 @@
+fish_vi_key_bindings
+
+begin
+    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
+
+# Base16 Shell
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
