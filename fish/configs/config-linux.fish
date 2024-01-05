ba02ce63df05ba81df34d6fd00da127c4fa1c223
adminn <adminn@pop-os.Dlink>
2024-01-05 21:01:51 +0800

    update.

--- fish/configs/config-linux.fish
+++ fish/configs/config-linux.fish
@@ -1,14 +1,19 @@
 if status --is-interactive
     set -g fish_greeting
     fish_vi_key_bindings
+
     starship init fish | source
-    fzf_configure_bindings --history=\cr --directory=\cf --git_log=\cg --git_status=\cs --processes=\cp
+
     export LC_ALL=C.UTF-8
     export LANG=C.UTF-8
     export BAT_THEME="ansi"
-    export LS_COLORS="$(vivid generate gruvbox-dark)"
+    export LS_COLORS=(vivid generate gruvbox-dark)
 end
 
-set fzf_preview_dir_cmd eza --grid --icons $argv
-set fzf_directory_opts --bind "ctrl-o:execute(nvim {} &> /dev/tty)"
-set -Ux STARSHIP_CONFIG ~/.config/starship/starship.toml
+set -g -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+
+xmodmap -e "keycode 64 = Mode_switch" # set Alt_l as the "Mode_switch"
+xmodmap -e "keycode 43 = h H Left H" # h
+xmodmap -e "keycode 44 = j J Down J" # j
+xmodmap -e "keycode 45 = k K Up K" # k
+xmodmap -e "keycode 46 = l L Right L" # l
