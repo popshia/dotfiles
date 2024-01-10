ecd1a7aaca5e992738f398376f29d89819314168
Noah Lin <noah@c-link.com.tw>
2024-01-10 10:33:17 +0800

    add ranger configs and update fish config.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,15 +6,30 @@ if status --is-interactive
 	zoxide init fish | source
 	fzf_configure_bindings --history=\cr --directory=\cf --git_log=\cg --git_status=\cs --processes=\cp
 
-    export LC_ALL=C.UTF-8
-    export LANG=C.UTF-8
-    export BAT_THEME="ansi"
-    export LS_COLORS=(vivid generate gruvbox-dark)
+    set -gx LC_ALL C.UTF-8
+    set -gx LANG C.UTF-8
+    set -gx BAT_THEME "ansi"
+    set -gx LS_COLORS (vivid generate gruvbox-dark)
+
+	set -gx EDITOR "nvim"
+	set -gx VISUAL "nvim"
+	set fzf_preview_dir_cmd eza --grid --icons $argv
+	set fzf_directory_opts --bind "L:execute(nvim {} &> /dev/tty)"
+	set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+
+	alias cp '$HOME/repos/advcpmv/advcp -g'
+	alias mv '$HOME/repos/advcpmv/advmv -g'
 end
 
-set fzf_preview_dir_cmd eza --grid --icons $argv
-set fzf_directory_opts --bind "ctrl-o:execute(nvim {} &> /dev/tty)"
-set -g -x STARSHIP_CONFIG ~/.config/starship/starship.toml
-
-alias cp '$HOME/repos/advcpmv/advcp -g'
-alias mv '$HOME/repos/advcpmv/advmv -g'
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+	if test -f /home/noah/miniconda3/bin/conda
+		eval /home/noah/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+	else
+		if test -f "/home/noah/miniconda3/etc/fish/conf.d/conda.fish"
+			. "/home/noah/miniconda3/etc/fish/conf.d/conda.fish"
+		else
+			set -x PATH "/home/noah/miniconda3/bin" $PATH
+		end
+	end
+# <<< conda initialize <<<
