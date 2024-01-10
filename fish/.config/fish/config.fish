b7d3e8210c6c5c442aea2b73a6eb0add3fc1e79b
Noah Lin <noah@c-link.com.tw>
2024-01-10 14:10:21 +0800

    update config.fish, add fish funtions, add extra packages.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,24 +1,27 @@
 if status --is-interactive
+	# remove fish_greeting
     set -g fish_greeting
-    fish_vi_key_bindings
 
+	# source packages
     starship init fish | source
 	zoxide init fish | source
+
+	# keybindings
+    fish_vi_key_bindings
 	fzf_configure_bindings --history=\cr --directory=\cf --git_log=\cg --git_status=\cs --processes=\cp
 
+	# fzf.fish
+	set fzf_preview_dir_cmd eza --grid --icons $argv
+	set fzf_directory_opts --bind "L:execute(nvim {} &> /dev/tty)"
+
+	# set variables
     set -gx LC_ALL C.UTF-8
     set -gx LANG C.UTF-8
     set -gx BAT_THEME "ansi"
     set -gx LS_COLORS (vivid generate gruvbox-dark)
-
 	set -gx EDITOR "nvim"
 	set -gx VISUAL "nvim"
-	set fzf_preview_dir_cmd eza --grid --icons $argv
-	set fzf_directory_opts --bind "L:execute(nvim {} &> /dev/tty)"
 	set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
-
-	alias cp '$HOME/repos/advcpmv/advcp -g'
-	alias mv '$HOME/repos/advcpmv/advmv -g'
 end
 
 # >>> conda initialize >>>
