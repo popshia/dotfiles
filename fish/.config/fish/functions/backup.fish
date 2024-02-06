e87a9276f64d7263929f78b05315cfe7843f9c97
Noah Lin <noah@c-link.com.tw>
2024-02-06 13:58:37 +0800

    add backup.fish for rsync.

---
+++ fish/.config/fish/functions/backup.fish
@@ -0,0 +1,9 @@
+function backup
+	set DATE (date +%Y%m%d)
+	set PROJ_DIR "/home/noah/projects"
+	set REPO_DIR "/home/noah/repos"
+	set NOTE_DIR "/home/noah/notes"
+	set DEST_DIR "/mnt/clink_ai/C-LINK AI@CORE/個人區/Noah/backup/"
+	set LOG_FILE "/mnt/clink_ai/C-LINK AI@CORE/共用區/資訊安全管理系統/2024_ISO/4_紀錄文件/每周備份專案資料紀錄/Noah/$DATE.log"
+	nohup rsync -avhiL $PROJ_DIR $REPO_DIR $NOTE_DIR $DEST_DIR --progress --log-file=$LOG_FILE &> ~/Documents/backup.log &
+end
