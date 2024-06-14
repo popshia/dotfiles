8e5fa3af26ad0ba22c803e7f3b955531f374f72d
Noah Lin <noah@c-link.com.tw>
2024-06-14 11:08:26 +0800

    updated git-delta and ll ls functions.

--- fish/.config/fish/functions/ll.fish
+++ fish/.config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll
-  eza --all --long --group-directories-first --git --icons $argv
+    eza --all --long --sort=ext --group-directories-first --git --icons $argv
 end
--- fish/.config/fish/functions/ls.fish
+++ fish/.config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
 function ls
-    eza --grid --group-directories-first --icons $argv
+    eza --grid --sort=ext --group-directories-first --icons $argv
     # exa --grid $argv
 end
