3a88e4f51bc298c03d0db104e03ab6980f8bdbc2
Noah lin <noah@c-link.com.tw>
2025-03-20 18:40:34 +0800

    feat(zed, orbstack): add orbstack fish completion and zed prediction configs.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -42,3 +42,7 @@ switch (uname)
         set -gx XMODIFIERS @im fcitx
         fish_add_path ~/.local/bin
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
